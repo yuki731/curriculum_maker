40b068345c2712e3f51fee72f1e9b331491dc3a3
dongmwu <i952030@gmail.com>
2025-06-06 03:40:54 +0900

    スキーマを修正した。ルータ作成中。

--- agent_api/routers/gen.py
+++ agent_api/routers/gen.py
@@ -1,6 +1,14 @@
 from fastapi import APIRouter
 import agent_api.schemas.gen as gen_schema
 from google import genai
+import curriculum_maker_api.api.youtube_to_db.getYoutube as getYoutube
+import curriculum_maker_api.api.youtube_to_db.updateChromaDB as updateChromaDB
+
+YOUTUBE_API_KEY = ""
+GEMINI_API_KEY = ""
+
+GEN_YOUTUBE_QUERY_PROMPT = """ユーザの要望に基づいて、YouTubeから適切な動画を選びたいです。YouTubeに入力する適切な検索キーワードを出力してください。以下はユーザの要望です。
+ユーザの要望："""
 
 router = APIRouter()
 
@@ -8,7 +16,14 @@ router = APIRouter()
 @router.post("/gen")
 async def gen(user_request: gen_schema.UserRequest):
     # gemma-3-27b-itを呼び出す。
-    #client = genai.Client(api_key="")
+    client = genai.Client(api_key=GEMINI_API_KEY)
+    youtube_query = client.models.generate_content(
+        model="gemma-3-27b-it",
+        contents=[f"{GEN_YOUTUBE_QUERY_PROMPT}{user_request.user_message}"]
+    )
+
+    df_youtube_data = getYoutube.getYoutubeData(YOUTUBE_API_KEY, youtube_query, 50)
+    
     #chat = client.chats.create(model="gemma-3-27b-it")
     #response = chat.send_message("こんにちは！")
     #response = chat.send_message("海老名駅の近くにあるおいしいお店を教えて！")
