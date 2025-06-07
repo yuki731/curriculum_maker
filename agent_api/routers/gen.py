8287fc837d71c7091dd0b10e3ccd27329d07dae4
dongmwu <i952030@gmail.com>
2025-06-08 03:25:49 +0900

    URLを返すため、タイトル to URLの辞書は必要。

--- agent_api/routers/gen.py
+++ agent_api/routers/gen.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 import agent_api.schemas.gen as gen_schema
 from google import genai
+import vertexai
+from google.cloud import aiplatform
 from langchain_google_vertexai import VertexAIEmbeddings, ChatVertexAI
 from langchain_chroma import Chroma
 import curriculum_maker_api.api.youtube_to_db.getYoutube as getYoutube
@@ -8,10 +10,12 @@ import curriculum_maker_api.api.youtube_to_db.updateChromaDB as updateChromaDB
 
 YOUTUBE_API_KEY = ""
 GEMINI_API_KEY = ""
+PROJECT_ID = ""
+REGION = "asia-northeast1"
 PERSIST_DIRECTORY = ""
-COLLECTION_NAME = "youtube_videos_vertex_ai"
+COLLECTION_NAME = "youtube_videos_vertex_ai_test20250607"
 
-GEN_YOUTUBE_QUERY_PROMPT = """ユーザの要望に基づいて、YouTubeから適切な動画を選びたいです。YouTubeに入力する適切な検索キーワードを出力してください。以下はユーザの要望です。
+GEN_YOUTUBE_QUERY_PROMPT = """ユーザの要望に基づいて、YouTubeから適切な動画を選びたいです。YouTubeに入力する適切な検索キーワードを出力してください。検索キーワードのみ出力してください。20文字以内にしてください。以下はユーザの要望です。
 ユーザの要望："""
 
 CURRICULUM_INSTRUCTION_PROMPT = """ユーザの要望に基づいて、YouTubeから以下の動画を候補として選びました。以下の動画を適切に選択し順番を考え、よいカリキュラムを組んでください。また、その順番にした理由も出力してください。カリキュラムと理由は下記の形式に沿って出力してください。カリキュラムと理由のみ出力してください。
@@ -29,6 +33,9 @@ CURRICULUM_FORMAT = """カリキュラム：
 ..."""
 REASON_FORMAT = """理由：......"""
 
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+vertexai.init(project=PROJECT_ID, location=REGION)
 embeddings = VertexAIEmbeddings(model_name="text-multilingual-embedding-002")
 try:
     vectorstore = Chroma(
@@ -52,15 +59,14 @@ router = APIRouter()
 
 @router.post("/gen")
 async def gen(user_request: gen_schema.UserRequest):
-    client = genai.Client(api_key=GEMINI_API_KEY)
     youtube_query = client.models.generate_content(
         model="gemma-3-27b-it",
         contents=[f"{GEN_YOUTUBE_QUERY_PROMPT}{user_request.user_message}"]
-    )
-
-    df_youtube_data = getYoutube.getYoutubeData(YOUTUBE_API_KEY, youtube_query, 50)
+    ).text
+    print(youtube_query)
+    df_youtube_data = getYoutube.getYoutubeData(YOUTUBE_API_KEY, youtube_query, 10)
     updateChromaDB.updateChromaDB(df_youtube_data, vectorstore)
-    search_results = vectorstore.similarity_search_with_score(youtube_query, k=3) # kは取得するドキュメント数
+    search_results = vectorstore.similarity_search_with_score(youtube_query, k=10) # kは取得するドキュメント数
 
     if search_results:
         videos = ""
@@ -82,14 +88,15 @@ async def gen(user_request: gen_schema.UserRequest):
         message = client.models.generate_content(
             model="gemma-3-27b-it",
             contents=[f"{CURRICULUM_INSTRUCTION_PROMPT}{videos}\n{CURRICULUM_FORMAT}\n\n{REASON_FORMAT}"]
-        )
+        ).text
     else:
         message = videos
     print(message)
 
     return gen_schema.ModelResponse(message=message,
                                     video_urls=["url1","url2"],
-                                    video_lengths=["0:00","0:00"])
+                                    video_lengths=["0:00","0:00"],
+                                    titles=["title1", "title2"])
 
 
 @router.get("/test")
@@ -97,4 +104,5 @@ async def test():
     return gen_schema.ModelResponse(message="以下はおすすめの動画一覧です。理由は～。",
                                     video_urls=["https://www.youtube.com/watch?v=jZqTz1G8G04",
                                                 "https://www.youtube.com/watch?v=nB2qTzs2HTk"],
-                                    video_lengths=["4:14","3:16"])
+                                    video_lengths=["4:14","3:16"],
+                                    titles=["title1", "title2"])
