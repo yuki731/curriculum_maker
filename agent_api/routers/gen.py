51e41f6a0e9099d90e05a7ea97a5e8ffde829efb
dongmwu <i952030@gmail.com>
2025-06-17 03:49:27 +0900

    markdown対応した。DRF対応した。

--- agent_api/routers/gen.py
+++ agent_api/routers/gen.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+import re
+from fastapi import APIRouter, HTTPException
+import httpx
 import agent_api.schemas.gen as gen_schema
 from google import genai
 import vertexai
@@ -14,26 +16,34 @@ PROJECT_ID = ""
 REGION = "asia-northeast1"
 PERSIST_DIRECTORY = ""
 COLLECTION_NAME = "youtube_videos_vertex_ai_test20250607"
+DRF_URL = ""
+
 
 GEN_YOUTUBE_QUERY_PROMPT = """ユーザの要望に基づいて、YouTubeから適切な動画を選びたいです。YouTubeに入力する適切な検索キーワードを出力してください。検索キーワードのみ出力してください。20文字以内にしてください。以下はユーザの要望です。
 ユーザの要望："""
 
-CURRICULUM_INSTRUCTION_PROMPT = """ユーザの要望に基づいて、YouTubeから以下の動画を候補として選びました。以下の動画を適切に選択し順番を考え、よいカリキュラムを組んでください。また、その順番にした理由も出力してください。カリキュラムと理由は下記の形式に沿って出力してください。カリキュラムと理由のみ出力してください。
-動画候補：
+CURRICULUM_INSTRUCTION_PROMPT = """ユーザの要望に基づいて、YouTubeから以下の動画候補を選びました。ユーザの要望に応えられるように、以下の動画候補から適切な動画を選択し順番を考え、よいカリキュラムを組んでください。また、その順番にした理由とカリキュラムのタイトルも考えてください。カリキュラムのタイトル、カリキュラム、理由は下記のMarkdown形式に沿って出力してください。カリキュラムのタイトル、カリキュラム、理由のみ出力してください。
+動画候補:
 """
-CURRICULUM_FORMAT = """カリキュラム：
-動画１
-タイトル：...
-動画説明：...
+CURRICULUM_FORMAT = """# タイトル:[実際のカリキュラムのタイトル]
 
-動画２
-タイトル：...
-動画説明：...
+# カリキュラム:
+## 動画１
+タイトル:...
+動画説明:...
 
-..."""
-REASON_FORMAT = """理由：......"""
+## 動画２
+タイトル:...
+動画説明:...
 
-client = genai.Client(api_key=GEMINI_API_KEY)
+（カリキュラムに含まれるすべての動画を上記のように出力してください。）
+
+
+# 理由:
+......"""
+
+
+genai_client = genai.Client(api_key=GEMINI_API_KEY)
 
 vertexai.init(project=PROJECT_ID, location=REGION)
 embeddings = VertexAIEmbeddings(model_name="text-multilingual-embedding-002")
@@ -59,7 +69,7 @@ router = APIRouter()
 
 @router.post("/gen")
 async def gen(user_request: gen_schema.UserRequest):
-    youtube_query = client.models.generate_content(
+    youtube_query = genai_client.models.generate_content(
         model="gemma-3-27b-it",
         contents=[f"{GEN_YOUTUBE_QUERY_PROMPT}{user_request.user_message}"]
     ).text
@@ -70,6 +80,7 @@ async def gen(user_request: gen_schema.UserRequest):
 
     if search_results:
         videos = ""
+        title_url_dict = {}
         for doc, score in search_results:
             print("-" * 20)
             print(f"スコア: {score}")
@@ -78,25 +89,46 @@ async def gen(user_request: gen_schema.UserRequest):
             print(f"URL: {doc.metadata.get('source', 'N/A')}")
             print(f"説明の冒頭: {doc.page_content[:150]}...") # 説明の冒頭を表示
             print(f"検索キーワード: {doc.metadata.get('search_keywords', 'N/A')}")
-            videos += f"タイトル: {doc.metadata.get('title', 'N/A')}\n"
-            videos += f"動画説明: {doc.page_content[:150]}\n\n\n"
+            videos += f"タイトル:{doc.metadata.get('title', 'N/A')}\n"
+            videos += f"動画説明:{doc.page_content[:150]}\n\n\n"
+            title_url_dict[doc.metadata.get('title', 'N/A')] = doc.metadata.get('source', 'N/A')
     else:
         print("関連するドキュメントは見つかりませんでした。")
         videos = "関連する動画は見つかりませんでした。"
 
     if videos != "関連する動画は見つかりませんでした。":
-        message = client.models.generate_content(
+        message = genai_client.models.generate_content(
             model="gemma-3-27b-it",
-            contents=[f"{CURRICULUM_INSTRUCTION_PROMPT}{videos}\n{CURRICULUM_FORMAT}\n\n{REASON_FORMAT}"]
+            contents=[f"{CURRICULUM_INSTRUCTION_PROMPT}{videos}\n{CURRICULUM_FORMAT}"]
         ).text
+        final_movie_titles = re.findall(r'タイトル:(.*?)\n動画説明', message) #list
+        print(final_movie_titles)
+        movies = [{"title":movie_title, "url":title_url_dict[movie_title]} for movie_title in final_movie_titles]
+        title = re.findall(r'# タイトル:(.*?)\n', message)[0]
     else:
         message = videos
+        movies = [{"title": "no title", "url": "no_url"}]
+        title = "カリキュラムのタイトルはありません"
     print(message)
+    print(movies)
+    print(title)
+    
+    headers = {"Content-Type": "application/json"}
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(DRF_URL, json={"title":title, "movies":movies, "message":message}, headers=headers, timeout=60.0)
+            response.raise_for_status()
+        except httpx.HTTPStatusError as exc:
+            raise HTTPException(
+                status_code=exc.response.status_code,
+                detail=f"Upstream API error: {exc.response.text}"
+            )
+        except httpx.RequestError as exc:
+            raise HTTPException(
+                status_code=502,
+                detail=f"Error connecting to upstream API: {exc}")
 
-    return gen_schema.ModelResponse(message=message,
-                                    video_urls=["url1","url2"],
-                                    video_lengths=["0:00","0:00"],
-                                    titles=["title1", "title2"])
+    return {"status": "success", "upstream_response": response.json()}
 
 
 @router.get("/test")
